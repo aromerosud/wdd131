6d10b699365ab782633e7bfacf0c5253d77fad1b
Arturo Romero <romerosud89@gmail.com>
2025-11-07 08:28:40 -0500

    W02 Assignment: Picture Album

---
+++ scripts/temples.js
@@ -0,0 +1,20 @@
+const tempButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+const titleSpan = document.querySelector("#course-title");
+const copyrightSymbol = "©";
+const currentYear = new Date().getFullYear();
+const copyrightText = `${copyrightSymbol} ${currentYear}`;
+
+document.getElementById("currentyeaar").textContent = copyrightText;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+tempButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	tempButton.classList.toggle("open");
+
+	if(navigation.classList.contains("open")){
+		titleSpan.style.display = "none";
+	}else{
+		titleSpan.style.display = "flex";
+	}	
+});
